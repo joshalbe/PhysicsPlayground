b6f0a15df9ed12b310368e73e26fe57458597744
Josh Albe <joshjalbe@gmail.com>
2021-08-26 12:14:44 -0500

    Created a box that changes its material
    The script WakeupMaterialBehavior changes the box's material based on the state of its rigidbody.

---
+++ Assets/Scripts/WakeupMaterialBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class WakeupMaterialBehavior : MonoBehaviour
+{
+    public Material awakeMaterial = null;
+    public Material asleepMaterial = null;
+
+    private Rigidbody _rigidbody = null;
+    private MeshRenderer _renderer = null;
+
+    private bool _materialIsAwake = false;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _renderer = GetComponent<MeshRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        //Set material to asleep if rigidbody is asleep
+        if (_materialIsAwake && _rigidbody.IsSleeping() && asleepMaterial)
+        {
+            _renderer.material = asleepMaterial;
+            _materialIsAwake = false;
+        }
+        //Set material to awake if rigidbody is awake
+        else if (!_materialIsAwake && !_rigidbody.IsSleeping() && awakeMaterial)
+        {
+            _renderer.material = awakeMaterial;
+            _materialIsAwake = true;
+            
+        }
+    }
+}
