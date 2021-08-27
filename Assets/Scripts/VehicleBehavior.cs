316b91635b1c4d212ce7424626e8fb918d4662ad
Josh Albe <joshjalbe@gmail.com>
2021-08-27 16:02:03 -0500

    Finished tank dynamics

---
+++ Assets/Scripts/VehicleBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleBehavior : MonoBehaviour
+{
+    public HingeJoint frontRight;
+    public HingeJoint frontLeft;
+    public HingeJoint rearRight;
+    public HingeJoint rearLeft;
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
