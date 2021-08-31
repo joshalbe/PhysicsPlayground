f1d296fc7b4e8479091bede307d8f64b8c3b8491
Josh Albe <joshjalbe@gmail.com>
2021-08-31 17:05:22 -0500

    Added maneuverable camera

---
+++ Assets/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float distanceFromTarget = 10.0f;
+    public float sensitivity = 1000.0f;
+    public bool invertY = false;
+
+    private void Update()
+    {
+        //Rotate the camera
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 angles = transform.eulerAngles;
+            Vector2 rotation;
+            rotation.x = Input.GetAxis("Mouse Y") * (invertY ? 1.0f : -1.0f);
+            rotation.y = Input.GetAxis("Mouse X");
+            //Look up and down by rotating around X-axis
+            angles.x = Mathf.Clamp(angles.x + rotation.x * sensitivity * Time.deltaTime, 0.0f, 70.0f);
+            //Look left and right by rotating around the Y-axis
+            angles.y += rotation.y * sensitivity * Time.deltaTime;
+            //Set the angles
+            transform.eulerAngles = angles;
+        }
+
+        //Move the camera
+        transform.position = target.position + (distanceFromTarget * -transform.forward);
+
+    }
+}
--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -6,13 +6,15 @@ public class PlayerBehavior : MonoBehaviour
 {
     public float speed = 5.0f;
     public float jumpStrength = 100.0f;
+    public float aircontrol = 1.0f;
     public float gravityModifier = 0.2f;
 
     private CharacterController _controller;
 
     private Vector3 _desiredVelocity;
     private Vector3 _airVelocity;
-    private bool _jumpIsDesired;
+    private bool _jumpIsDesired = false;
+    private bool _isGrounded = false;
 
     private void Awake()
     {
@@ -29,20 +31,31 @@ public class PlayerBehavior : MonoBehaviour
         //Get jump input
         _jumpIsDesired = Input.GetButtonDown("Jump");
 
+        //Apply air control
+
+
         //Set movement magnitude
         _desiredVelocity.Normalize();
         _desiredVelocity *= speed;
 
+        //Check for ground
+        _isGrounded = _controller.isGrounded;
+
         //Apply jump strength
-        if (_jumpIsDesired)
+        if (_jumpIsDesired && _isGrounded)
         {
             _airVelocity.y = jumpStrength;
             _jumpIsDesired = false;
         }
 
+        //Stop on ground
+        if (_isGrounded && _airVelocity.y < 0.0f)
+        {
+            _airVelocity.y = -1.0f;
+        }
+
         //Apply gravity
         _airVelocity += Physics.gravity * gravityModifier * Time.deltaTime;
-
 
         //Move
         _desiredVelocity += _airVelocity;
