beb5db0340c8fa8f581a9e0d810c84f37736567d
Josh Albe <joshjalbe@gmail.com>
2021-09-14 12:43:11 -0500

    Optimized movement code

--- Assets/Scripts/TankBehavior.cs
+++ Assets/Scripts/TankBehavior.cs
@@ -53,6 +53,11 @@ public class TankBehavior : MonoBehaviour
     private JointMotor _urMotor;
     private JointMotor _ulMotor;
 
+    private HingeJoint[] _tankHingesR = new HingeJoint[7];
+    private HingeJoint[] _tankHingesL = new HingeJoint[7];
+    private JointMotor[] _tankMotorsR = new JointMotor[7];
+    private JointMotor[] _tankMotorsL = new JointMotor[7];
+
     private void Awake()
     {
         _tankController = GetComponent<CharacterController>();
@@ -61,7 +66,7 @@ public class TankBehavior : MonoBehaviour
         _hingeJoints = GetComponents<HingeJoint>();
 
         //Individually assign each joint to its proper pre-existing partner
-        //as well as assing the motors of each joint
+        //as well as assign the motors of each joint
         _1RightWheel = _hingeJoints[0];
         _1rMotor = _1RightWheel.motor;
 
@@ -104,10 +109,25 @@ public class TankBehavior : MonoBehaviour
         _upLeftWheel = _hingeJoints[13];
         _ulMotor = _upLeftWheel.motor;
 
+        _tankHingesR[0] = _frontRightWheel; _tankHingesR[1] = _1RightWheel; _tankHingesR[2] = _2RightWheel;
+        _tankHingesR[3] = _3RightWheel; _tankHingesR[4] = _4RightWheel; _tankHingesR[5] = _5RightWheel;
+        _tankHingesR[6] = _upRightWheel;
+
+        _tankHingesL[0] = _frontLeftWheel; _tankHingesL[1] = _1LeftWheel; _tankHingesL[2] = _2LeftWheel;
+        _tankHingesL[3] = _3LeftWheel; _tankHingesL[4] = _4LeftWheel; _tankHingesL[5] = _5LeftWheel;
+        _tankHingesL[6] = _upLeftWheel;
+
+        _tankMotorsR[0] = _flMotor; _tankMotorsR[1] = _1rMotor; _tankMotorsR[2] = _2rMotor; _tankMotorsR[3] = _3rMotor;
+        _tankMotorsR[4] = _4rMotor; _tankMotorsR[5] = _5rMotor; _tankMotorsR[6] = _urMotor;
+
+        _tankMotorsL[0] = _flMotor; _tankMotorsL[1] = _1lMotor; _tankMotorsL[2] = _2lMotor; _tankMotorsL[3] = _3lMotor;
+        _tankMotorsL[4] = _4lMotor; _tankMotorsL[5] = _5lMotor; _tankMotorsL[6] = _ulMotor;
     }
 
     private void Update()
     {
+        GoForward(1000.0f, 300.0f);
+
         _goingForward = Input.GetButtonDown("Up");
         _goingBack = Input.GetButtonDown("Down");
         _goingRight = Input.GetButtonDown("Right");
@@ -115,278 +135,103 @@ public class TankBehavior : MonoBehaviour
 
         if (_goingForward)
         {
-            GoForward();
+            GoForward(1000.0f, 300.0f);
         }
         else if (_goingBack)
         {
-            GoBackwards();
+            GoBackwards(1000.0f, 300.0f);
         }
         else if (_goingRight)
         {
-            TurnRight();
+            TurnRight(1000.0f, 300.0f);
         }
         else if (_goingLeft)
         {
-            TurnLeft();
+            TurnLeft(1000.0f, 300.0f);
         }
         else
         {
-            TankStop();
+            TankStop(0.0f, 30.0f);
         }
     }
 
-    private void TankStop()
+    private void TankStop(float velocity, float force)
     {
-        _frMotor.targetVelocity = 0.0f;
-        _1rMotor.targetVelocity = 0.0f;
-        _2rMotor.targetVelocity = 0.0f;
-        _3rMotor.targetVelocity = 0.0f;
-        _4rMotor.targetVelocity = 0.0f;
-        _5rMotor.targetVelocity = 0.0f;
-        _urMotor.targetVelocity = 0.0f;
-
-        _flMotor.targetVelocity = 0.0f;
-        _1lMotor.targetVelocity = 0.0f;
-        _2lMotor.targetVelocity = 0.0f;
-        _3lMotor.targetVelocity = 0.0f;
-        _4lMotor.targetVelocity = 0.0f;
-        _5lMotor.targetVelocity = 0.0f;
-        _ulMotor.targetVelocity = 0.0f;
-
-        _frMotor.force = 30.0f;
-        _1rMotor.force = 30.0f;
-        _2rMotor.force = 30.0f;
-        _3rMotor.force = 30.0f;
-        _4rMotor.force = 30.0f;
-        _5rMotor.force = 30.0f;
-        _urMotor.force = 30.0f;
-
-        _flMotor.force = 30.0f;
-        _1lMotor.force = 30.0f;
-        _2lMotor.force = 30.0f;
-        _3lMotor.force = 30.0f;
-        _4lMotor.force = 30.0f;
-        _5lMotor.force = 30.0f;
-        _ulMotor.force = 30.0f;
-
-        _frontRightWheel.motor = _frMotor;
-        _1RightWheel.motor = _1rMotor;
-        _2RightWheel.motor = _2rMotor;
-        _3RightWheel.motor = _3rMotor;
-        _4RightWheel.motor = _4rMotor;
-        _5RightWheel.motor = _5rMotor;
-        _upRightWheel.motor = _urMotor;
-
-        _frontLeftWheel.motor = _flMotor;
-        _1LeftWheel.motor = _1lMotor;
-        _2LeftWheel.motor = _2lMotor;
-        _3LeftWheel.motor = _3lMotor;
-        _4LeftWheel.motor = _4lMotor;
-        _5LeftWheel.motor = _5lMotor;
-        _upLeftWheel.motor = _ulMotor;
+        for (int i = 0; i < _tankMotorsR.Length - 1; i++)
+        {
+            _tankMotorsR[i].targetVelocity = velocity;
+            _tankMotorsR[i].force = force;
+            _tankHingesR[i].motor = _tankMotorsR[i];
+        }
+        for (int i = 0; i < _tankMotorsL.Length - 1; i++)
+        {
+            _tankMotorsL[i].targetVelocity = velocity;
+            _tankMotorsL[i].force = force;
+            _tankHingesL[i].motor = _tankMotorsL[i];
+        }
     }
 
-    private void TurnRight()
+    private void TurnRight(float velocity, float force)
     {
-        _frMotor.targetVelocity = 1000.0f;
-        _1rMotor.targetVelocity = 1000.0f;
-        _2rMotor.targetVelocity = 1000.0f;
-        _3rMotor.targetVelocity = 1000.0f;
-        _4rMotor.targetVelocity = 1000.0f;
-        _5rMotor.targetVelocity = 1000.0f;
-        _urMotor.targetVelocity = 1000.0f;
-
-        _flMotor.targetVelocity = 1000.0f;
-        _1lMotor.targetVelocity = 1000.0f;
-        _2lMotor.targetVelocity = 1000.0f;
-        _3lMotor.targetVelocity = 1000.0f;
-        _4lMotor.targetVelocity = 1000.0f;
-        _5lMotor.targetVelocity = 1000.0f;
-        _ulMotor.targetVelocity = 1000.0f;
-
-        _frMotor.force = 300.0f;
-        _1rMotor.force = 300.0f;
-        _2rMotor.force = 300.0f;
-        _3rMotor.force = 300.0f;
-        _4rMotor.force = 300.0f;
-        _5rMotor.force = 300.0f;
-        _urMotor.force = 300.0f;
-
-        _flMotor.force = 300.0f;
-        _1lMotor.force = 300.0f;
-        _2lMotor.force = 300.0f;
-        _3lMotor.force = 300.0f;
-        _4lMotor.force = 300.0f;
-        _5lMotor.force = 300.0f;
-        _ulMotor.force = 300.0f;
-
-        _frontRightWheel.motor = _frMotor;
-        _1RightWheel.motor = _1rMotor;
-        _2RightWheel.motor = _2rMotor;
-        _3RightWheel.motor = _3rMotor;
-        _4RightWheel.motor = _4rMotor;
-        _5RightWheel.motor = _5rMotor;
-        _upRightWheel.motor = _urMotor;
-
-        _frontLeftWheel.motor = _flMotor;
-        _1LeftWheel.motor = _1lMotor;
-        _2LeftWheel.motor = _2lMotor;
-        _3LeftWheel.motor = _3lMotor;
-        _4LeftWheel.motor = _4lMotor;
-        _5LeftWheel.motor = _5lMotor;
-        _upLeftWheel.motor = _ulMotor;
+        for (int i = 0; i < _tankMotorsR.Length - 1; i++)
+        {
+            _tankMotorsR[i].targetVelocity = velocity;
+            _tankMotorsR[i].force = force;
+            _tankHingesR[i].motor = _tankMotorsR[i];
+        }
+        for (int i = 0; i < _tankMotorsL.Length - 1; i++)
+        {
+            _tankMotorsL[i].targetVelocity = velocity;
+            _tankMotorsL[i].force = force;
+            _tankHingesL[i].motor = _tankMotorsL[i];
+        }
     }
 
-    private void TurnLeft()
+    private void TurnLeft(float velocity, float force)
     {
-        _frMotor.targetVelocity = -1000.0f;
-        _1rMotor.targetVelocity = -1000.0f;
-        _2rMotor.targetVelocity = -1000.0f;
-        _3rMotor.targetVelocity = -1000.0f;
-        _4rMotor.targetVelocity = -1000.0f;
-        _5rMotor.targetVelocity = -1000.0f;
-        _urMotor.targetVelocity = -1000.0f;
-
-        _flMotor.targetVelocity = -1000.0f;
-        _1lMotor.targetVelocity = -1000.0f;
-        _2lMotor.targetVelocity = -1000.0f;
-        _3lMotor.targetVelocity = -1000.0f;
-        _4lMotor.targetVelocity = -1000.0f;
-        _5lMotor.targetVelocity = -1000.0f;
-        _ulMotor.targetVelocity = -1000.0f;
-
-        _frMotor.force = 300.0f;
-        _1rMotor.force = 300.0f;
-        _2rMotor.force = 300.0f;
-        _3rMotor.force = 300.0f;
-        _4rMotor.force = 300.0f;
-        _5rMotor.force = 300.0f;
-        _urMotor.force = 300.0f;
-
-        _flMotor.force = 300.0f;
-        _1lMotor.force = 300.0f;
-        _2lMotor.force = 300.0f;
-        _3lMotor.force = 300.0f;
-        _4lMotor.force = 300.0f;
-        _5lMotor.force = 300.0f;
-        _ulMotor.force = 300.0f;
-
-        _frontRightWheel.motor = _frMotor;
-        _1RightWheel.motor = _1rMotor;
-        _2RightWheel.motor = _2rMotor;
-        _3RightWheel.motor = _3rMotor;
-        _4RightWheel.motor = _4rMotor;
-        _5RightWheel.motor = _5rMotor;
-        _upRightWheel.motor = _urMotor;
-
-        _frontLeftWheel.motor = _flMotor;
-        _1LeftWheel.motor = _1lMotor;
-        _2LeftWheel.motor = _2lMotor;
-        _3LeftWheel.motor = _3lMotor;
-        _4LeftWheel.motor = _4lMotor;
-        _5LeftWheel.motor = _5lMotor;
-        _upLeftWheel.motor = _ulMotor;
+        for (int i = 0; i < _tankMotorsR.Length - 1; i++)
+        {
+            _tankMotorsR[i].targetVelocity = -velocity;
+            _tankMotorsR[i].force = force;
+            _tankHingesR[i].motor = _tankMotorsR[i];
+        }
+        for (int i = 0; i < _tankMotorsL.Length - 1; i++)
+        {
+            _tankMotorsL[i].targetVelocity = -velocity;
+            _tankMotorsL[i].force = force;
+            _tankHingesL[i].motor = _tankMotorsL[i];
+        }
     }
 
-    private void GoForward()
+    private void GoForward(float velocity, float force)
     {
-        _frMotor.targetVelocity = -1000.0f;
-        _1rMotor.targetVelocity = -1000.0f;
-        _2rMotor.targetVelocity = -1000.0f;
-        _3rMotor.targetVelocity = -1000.0f;
-        _4rMotor.targetVelocity = -1000.0f;
-        _5rMotor.targetVelocity = -1000.0f;
-        _urMotor.targetVelocity = -1000.0f;
-
-        _flMotor.targetVelocity = 1000.0f;
-        _1lMotor.targetVelocity = 1000.0f;
-        _2lMotor.targetVelocity = 1000.0f;
-        _3lMotor.targetVelocity = 1000.0f;
-        _4lMotor.targetVelocity = 1000.0f;
-        _5lMotor.targetVelocity = 1000.0f;
-        _ulMotor.targetVelocity = 1000.0f;
-
-        _frMotor.force = 300.0f;
-        _1rMotor.force = 300.0f;
-        _2rMotor.force = 300.0f;
-        _3rMotor.force = 300.0f;
-        _4rMotor.force = 300.0f;
-        _5rMotor.force = 300.0f;
-        _urMotor.force = 300.0f;
-
-        _flMotor.force = 300.0f;
-        _1lMotor.force = 300.0f;
-        _2lMotor.force = 300.0f;
-        _3lMotor.force = 300.0f;
-        _4lMotor.force = 300.0f;
-        _5lMotor.force = 300.0f;
-        _ulMotor.force = 300.0f;
-
-        _frontRightWheel.motor = _frMotor;
-        _1RightWheel.motor = _1rMotor;
-        _2RightWheel.motor = _2rMotor;
-        _3RightWheel.motor = _3rMotor;
-        _4RightWheel.motor = _4rMotor;
-        _5RightWheel.motor = _5rMotor;
-        _upRightWheel.motor = _urMotor;
-
-        _frontLeftWheel.motor = _flMotor;
-        _1LeftWheel.motor = _1lMotor;
-        _2LeftWheel.motor = _2lMotor;
-        _3LeftWheel.motor = _3lMotor;
-        _4LeftWheel.motor = _4lMotor;
-        _5LeftWheel.motor = _5lMotor;
-        _upLeftWheel.motor = _ulMotor;
+        for (int i = 0; i < _tankMotorsR.Length - 1; i++)
+        {
+            _tankMotorsR[i].targetVelocity = -velocity;
+            _tankMotorsR[i].force = force;
+            _tankHingesR[i].motor = _tankMotorsR[i];
+        }
+        for (int i = 0; i < _tankMotorsL.Length - 1; i++)
+        {
+            _tankMotorsL[i].targetVelocity = velocity;
+            _tankMotorsL[i].force = force;
+            _tankHingesL[i].motor = _tankMotorsL[i];
+        }
     }
 
-    private void GoBackwards()
+    private void GoBackwards(float velocity, float force)
     {
-        _frMotor.targetVelocity = 1000.0f;
-        _1rMotor.targetVelocity = 1000.0f;
-        _2rMotor.targetVelocity = 1000.0f;
-        _3rMotor.targetVelocity = 1000.0f;
-        _4rMotor.targetVelocity = 1000.0f;
-        _5rMotor.targetVelocity = 1000.0f;
-        _urMotor.targetVelocity = 1000.0f;
-
-        _flMotor.targetVelocity = -1000.0f;
-        _1lMotor.targetVelocity = -1000.0f;
-        _2lMotor.targetVelocity = -1000.0f;
-        _3lMotor.targetVelocity = -1000.0f;
-        _4lMotor.targetVelocity = -1000.0f;
-        _5lMotor.targetVelocity = -1000.0f;
-        _ulMotor.targetVelocity = -1000.0f;
-
-        _frMotor.force = 300.0f;
-        _1rMotor.force = 300.0f;
-        _2rMotor.force = 300.0f;
-        _3rMotor.force = 300.0f;
-        _4rMotor.force = 300.0f;
-        _5rMotor.force = 300.0f;
-        _urMotor.force = 300.0f;
-
-        _flMotor.force = 300.0f;
-        _1lMotor.force = 300.0f;
-        _2lMotor.force = 300.0f;
-        _3lMotor.force = 300.0f;
-        _4lMotor.force = 300.0f;
-        _5lMotor.force = 300.0f;
-        _ulMotor.force = 300.0f;
-
-        _frontRightWheel.motor = _frMotor;
-        _1RightWheel.motor = _1rMotor;
-        _2RightWheel.motor = _2rMotor;
-        _3RightWheel.motor = _3rMotor;
-        _4RightWheel.motor = _4rMotor;
-        _5RightWheel.motor = _5rMotor;
-        _upRightWheel.motor = _urMotor;
-
-        _frontLeftWheel.motor = _flMotor;
-        _1LeftWheel.motor = _1lMotor;
-        _2LeftWheel.motor = _2lMotor;
-        _3LeftWheel.motor = _3lMotor;
-        _4LeftWheel.motor = _4lMotor;
-        _5LeftWheel.motor = _5lMotor;
-        _upLeftWheel.motor = _ulMotor;
+        for (int i = 0; i < _tankMotorsR.Length - 1; i++)
+        {
+            _tankMotorsR[i].targetVelocity = velocity;
+            _tankMotorsR[i].force = force;
+            _tankHingesR[i].motor = _tankMotorsR[i];
+        }
+        for (int i = 0; i < _tankMotorsL.Length - 1; i++)
+        {
+            _tankMotorsL[i].targetVelocity = -velocity;
+            _tankMotorsL[i].force = force;
+            _tankHingesL[i].motor = _tankMotorsL[i];
+        }
     }
 }
